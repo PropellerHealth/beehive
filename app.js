65c28ff340010df7a5ce8cca98b10a7b9a840143
Katie Sweet <katie.sweet@propellerhealth.com>
2020-11-06 17:28:11 -0600

    Add comments and fix log message

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Basic in-memory visits storage array
+// Create visits array as basic in-memory storage
 app.visits = [];
 
+// Create 'Add Visits' API endpoint
 app.post('/api/visits', function(req, res){
 
     // Store the supplied visit data
@@ -13,9 +14,11 @@ app.post('/api/visits', function(req, res){
     var visitId = app.visits.length;
     console.log('Stored visit count: ' + visitId);
 
-    res.status(201).json({'id':visitId});
+    // send the response
+    res.status(201).json({'id': visitId});
 });
 
+// Start app
 app.listen(port, () => {
-    console.log('Beehive API listening on port 3000');
+    console.log(`Beehive API listening on port ${port}`);
 });
